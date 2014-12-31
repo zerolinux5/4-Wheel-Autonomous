65db7949d60da50478a4dffa40c824822aebb4c3
Jesus Magana <maganazero5@gmail.com>
2014-12-30 19:09:34 -0800

    Added wire connections

--- _4wheelAuto.ino
+++ _4wheelAuto.ino
@@ -1,5 +1,28 @@
 //Needs to be on pulse width module input
-const int EN1;
-const int EN2;
-const int EN3;
-const int EN4;
+const int EN1 = 3; //Back Right Tire
+const int EN2 = 5; //Front Right Tire
+const int EN3 = 9; //Front Left Tire
+const int EN4 = 10; //Back Left Tire
+
+//Is only digital input
+const int MCL1 = 2; //Front Right Tire Set
+const int MCL2 = 7;
+
+const int MCL3 = 4; //Back Right Tire Set
+const int MCL4 = 6;
+
+const int MCR1 = 12; //Front Left Tire Set
+const int MCR2 = 13;
+
+const int MCR3 = 11; //Back Left Tire Set
+const int MCR4 = 8;
+
+void setup()
+{
+  
+}
+
+void loop()
+{
+  
+}
